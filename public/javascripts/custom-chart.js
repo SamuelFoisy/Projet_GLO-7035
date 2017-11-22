8c5575bb4616d7d218099e8a2e8c3a60f3ee638e
SamuelFoisy <samuel.foisy.1@ulaval.ca>
2017-11-21 20:08:03 -0500

    Preparation graphiques

--- public/javascripts/custom-chart.js
+++ public/javascripts/custom-chart.js
@@ -82,3 +82,63 @@ let houseTypeCountPieChart = generateChart('#houseTypeCount', {
         }
     }
 });
+
+
+function generatePieChart(selector, labels, options) {
+    generateChart(selector, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Prix',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Prix moyens',
+                fontSize: 22,
+                fontColor: '#000'
+            },
+            legend: {
+                display: true,
+                position: 'bottom'
+            }
+        }
+    });
+}
+
+/**
+ Requis:
+ Title
+ Type
+ Liste de libellés avec valeurs
+ Indicateur afficher légende
+ Indicateur afficher tooltips
+
+ Générer:
+ Couleurs des graphiques
+ Couleurs des bordures
+ Taille des polices
+ Position légende
+
+ */
+
